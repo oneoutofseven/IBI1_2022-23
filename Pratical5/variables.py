77e4850b577eb18c690cc1cf9f7ce9fe415bf429
Xu Chenzhi <chenzhi.22@intl.zju.edu.cn>
2023-03-15 17:04:05 +0800

    variables.py

--- Pratical5/variables.py
+++ Pratical5/variables.py
@@ -4,7 +4,14 @@ c=116.39
 d=abs(b-a)
 e=abs(c-a)
 if e>d:
-    print ("e is greater. the distance to Haining is farther")
+    print ("e is greater. the longitude distance to Haining is farther")
 else:
-    print ("d is greater. the distance to Los Angeles farther") 
+    print ("d is greater. the longitude distance to Los Angeles farther") 
+
+X=True
+Y=False
+W=X and Y
+Z=X or Y
+print ("W :",W)
+print ("Z :",Z)
 
