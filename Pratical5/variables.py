45928b0511995c90d4b2b11cbd675c433cfaeeda
Xu Chenzhi <chenzhi.22@intl.zju.edu.cn>
2023-03-15 16:15:22 +0800

    Pratical5

---
+++ Pratical5/variables.py
@@ -0,0 +1,10 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=abs(b-a)
+e=abs(c-a)
+if e>d:
+    print ("e is greater. the distance to Haining is farther")
+else:
+    print ("d is greater. the distance to Los Angeles farther") 
+
