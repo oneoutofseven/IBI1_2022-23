2a3cc0208fb63b7225b3f10fffdb3924b605be80
Xu Chenzhi <chenzhi.22@intl.zju.edu.cn>
2023-03-15 18:48:03 +0800

    hexagons.py

---
+++ Pratical5/hexagons.py
@@ -0,0 +1,11 @@
+#aim: calculation
+#n is all integars from 1 to 5
+#h=2n(2n-1)/2
+# Range() and "for in" are needed
+
+for n in range(1,6):
+  h=2*n*(2*n-1)/2
+  print ("the %d hexagon number is %d" % (n,h))
+
+
+
