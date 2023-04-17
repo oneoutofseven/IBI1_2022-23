f588252e8048bd8847eb74a1dae968c590bd171a
Xu Chenzhi <chenzhi.22@intl.zju.edu.cn>
2023-04-17 16:43:01 +0800

    make a change of the Olympics plot

--- Pratical6/Olympics_costs.py
+++ Pratical6/Olympics_costs.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+d={"Los Angeles 1984":1,"Seoul 1988":8,"Barcelona 1992":15,"Atlanta 1996":7,"Sydney 2000":5,"Athens 2004":14,"Beijing 2008":43,"London 2012":40}
+
 c=[1,8,15,7,5,14,43,40]  #costs list
 costs=sorted(c)
 print (costs)
 
-x = np.array(["Los Angeles 1984", "Sydney 2000", "Atlanta 1996", "Seoul 1988", "Athens 2004", "Barcelona 1992", "London 2012", "Beijing 2008"])
+D=sorted(d.items(),key = lambda x:x[1],reverse=False) #arrange the cities corresponding to "cost" list
+city_costs=dict(D)
+city=list(city_costs.keys())
+x = np.array(city)
 y = np.array(costs)
 
-plt.barh(x,y)  #form the bar chart
+plt.title("costs of the Olympics")
+plt.ylabel("costs/$billions")
+plt.bar(x,y)  #form the bar chart
+plt.xticks(x,rotation=-60)
 plt.show()
