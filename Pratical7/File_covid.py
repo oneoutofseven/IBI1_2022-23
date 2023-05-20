75d66475445ba5cfe13731f7f6bbf41a360a708e
Xu Chenzhi <chenzhi.22@intl.zju.edu.cn>
2023-05-20 15:26:05 +0800

    modify the code

--- Pratical7/File_covid.py
+++ Pratical7/File_covid.py
@@ -4,24 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #correct code for showing the second column from every 100th row from the first 1000 rows
+os.chdir("C:/users/xuchenzhi/IBI1_2022-23/IBI1_2022-23/Pratical7")
 df = pd.read_csv("full_data.csv")
 df.iloc[0:1100:100,1] #list the numbers
 print (df.iloc[0:1100:100,1])
 
 #a Boolean to show “total cases” for all rows corresponding to Afghanistan
-is_Afghanistan= df["location"]=="Afghanistan" #Boolean distinguish
+#Boolean distinguish
 print (df.loc[df["location"]=="Afghanistan","total_cases"])
 
 # mean number of new cases and new deaths on 31 March 2020
-newcases = df.loc[df.date=="2020-03-31","new_cases"] #make a dataframe of the new cases on 2020-03-31
-newdeaths = df.loc[df.date=="2020-03-31","new_deaths"] #make a dataframe of the new deaths on 2020-03-31
+df1=df.loc[df.location != "World",:] #exclude the "World"
+newcases = df1.loc[df1.date=="2020-03-31","new_cases"] #make a dataframe of the new cases on 2020-03-31
+newdeaths = df1.loc[df1.date=="2020-03-31","new_deaths"] #make a dataframe of the new deaths on 2020-03-31
 print (newcases)
 print ("Mean Number of New Cases on 2020-03-31 is", np.mean(newcases)) #caculate the mean
 print (newdeaths)
 print ("Mean Number of New Deaths on 2020-03-31 is", np.mean(newdeaths))
 
 #boxplot of new cases and new deaths on 31 March 2020
-world_data= df.loc[df.date=="2020-03-31",["new_cases","new_deaths"]]
+world_data= df1.loc[df1.date=="2020-03-31",["new_cases","new_deaths"]]
 print (world_data)
 plt.boxplot([world_data.iloc[:,0],world_data.iloc[:,1]],labels=['new_cases','new_deaths']) #make the boxplot 
 plt.title("world data on 2020-03-31")
@@ -43,7 +45,7 @@ y = df.loc[df.location == "World", "new_deaths"]
 plt.figure(figsize=(10,10))
 plt.plot(x, y, "b+")
 plt.xticks(x.iloc[0:len(x):4], rotation=-90)
-plt.title("new cases worldwide over time")
+plt.title("new deaths worldwide over time")
 plt.ylabel("population")
 plt.show()
 
