f932ad0048412e5c289cc5577ef4e6085f5fb50f
Xu Chenzhi <chenzhi.22@intl.zju.edu.cn>
2023-05-20 14:52:46 +0800

    modify the code to print the dictionary

--- Pratical6/Favourite_movie_genre.py
+++ Pratical6/Favourite_movie_genre.py
@@ -1,11 +1,11 @@
 tinydict = {"Comedy":73,"Action":42,"Romance":38,"Fantasy":28,"Science-fiction":22,"Horror":19,"Crime":18,"Documentary":12,"History":8,"War":7}
 #create the dictionary of "favourite movie genre"
+print(tinydict)
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 requested_genre = "Comedy"
-
 print ("The number of students who prefer", requested_genre, "is :",tinydict[requested_genre])
 
 labels=tinydict.keys() #genre
