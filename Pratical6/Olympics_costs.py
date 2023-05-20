e5da0e6a0a10ea191d7e6df2e8b98ff600ee3e07
Xu Chenzhi <chenzhi.22@intl.zju.edu.cn>
2023-05-20 14:54:46 +0800

    modify Olympics_costs.py

--- Pratical6/Olympics_costs.py
+++ Pratical6/Olympics_costs.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 d={"Los Angeles 1984":1,"Seoul 1988":8,"Barcelona 1992":15,"Atlanta 1996":7,"Sydney 2000":5,"Athens 2004":14,"Beijing 2008":43,"London 2012":40}
+print(d)
 
 c=[1,8,15,7,5,14,43,40]  #costs list
 costs=sorted(c)
